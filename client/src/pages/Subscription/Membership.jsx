f8b5a7aad2cb7d9d4cbaf6d20f0b56559e7d2c30
deepanshi8006 <157835033+deepanshi8006@users.noreply.github.com>
2024-12-24 15:38:38 +0530

    Update Membership.jsx

--- client/src/pages/Subscription/Membership.jsx
+++ client/src/pages/Subscription/Membership.jsx
@@ -19,7 +19,7 @@ function Membership() {
     try {
       const currentTime = new Date();
     const start = new Date().setHours(10, 0, 0); // 10:00 AM IST
-    const end = new Date().setHours(12, 0, 0); // 11:00 AM IST
+    const end = new Date().setHours(11, 0, 0); // 11:00 AM IST
     if (currentTime >= start && currentTime <= end) {
       if (user) {
         const price = 100;
